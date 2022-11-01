a50da41d157b6a9a74c6335ff321fbaed5aa9857
anaskhan28 <khananasatique@gmail.com>
2022-11-01 11:38:35 +0530

    initial commit

---
+++ quizz-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Menu from './components/Menu';
+function App() {
+  return (
+    <div className="App">
+      <h1>Quiz App</h1>
+      <Menu/>
+    </div>
+  );
+}
+
+export default App;
---
+++ quizz-app/src/components/Menu.js
@@ -0,0 +1,9 @@
+import "../App.css"
+function Menu(){
+    return(
+    <div className="menu">
+        <input type="text" placeholder = "Ex. John Smith"/>
+        <button>Start Quiz</button>
+    </div>
+)} 
+export default Menu;
