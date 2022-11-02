b179e7fd430412fd2cb2b2e1e9abc15daee8548a
anaskhan28 <khananasatique@gmail.com>
2022-11-02 12:20:21 +0530

    completed space news

--- space-news/src/App.js
+++ space-news/src/App.js
@@ -1,16 +1,36 @@
 import './App.css';
-import {useEffect} from 'react';
+import {useEffect,useState} from 'react';
 
 function App() {
-
+  const [newsList, setNewsList] = useState([]);
   useEffect(()=>{
     fetch("https://api.spaceflightnewsapi.net/v3/articles")
     .then((response)=> response.json()).then((data) =>{
-          console.log(data);
+          setNewsList(data)
+          console.log(data)
     });
-  });
+  },[]);
+
+
   return (
     <div className="App">
+      <div className='title'>
+        <h1>Space News</h1>
+      </div>
+      <div className='newsContainer'>
+          {newsList.map((val,key) =>{
+            return <div className='article' 
+            onClick={()=>{
+              window.open(val.url, "_blank")
+            }} 
+            key={key}>
+              <h3>{val.title}</h3>
+              <img src={val.imageUrl} alt="space-img"/>
+              <p>{val.summary}</p>
+              <h4>{val.publishedAt}</h4>
+            </div>
+          })}
+      </div>
     </div>
   );
 }
