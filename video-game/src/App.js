e88b316771ae8b0eea6c14adb273e46d17c8e239
anaskhan28 <khananasatique@gmail.com>
2022-11-06 11:17:54 +0530

    completed

--- video-game/src/App.js
+++ video-game/src/App.js
@@ -1,18 +1,31 @@
 import './App.css';
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 
 function App() {
   
   const [gameTitle, setGameTitle] = useState('');
   const [searchGames, setSearchGames] = useState([]);
+  const [gameDeal, setGameDeal] = useState([]);
 
   const searchGame = ()=>{
     fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}&limit=3`)
     .then((response)=> response.json())
     .then((data) =>{
+      console.log(data)
       setSearchGames(data)
-    })
-  }
+      
+    });
+  };
+
+  useEffect(()=>{
+      fetch(`https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15&pageSize=3`)
+      .then((response)=>response.json())
+      .then((data)=>{
+        console.log(data)
+        setGameDeal(data)
+      })
+  }, []);
+ 
   return (
     <div className="App">
     <div className = "searchSection">
@@ -25,8 +38,12 @@ function App() {
         {searchGames.map((game, key)=>{
           return(
             <div className='game' key={key}>
-              
-            {game.external}
+            <h3>{game.external}</h3>
+            
+            <div>
+            <img src={game.thumb} alt="game"/>
+            <h4>${game.cheapest}</h4>
+            </div>
             </div>
           )
         })}
@@ -34,6 +51,22 @@ function App() {
     </div>
     <div className="dealsSection">
       <h1>Latest Deals 🔥</h1>
+      <div className='games deals'>
+      {gameDeal.map((deal,key)=>{
+        return <div className='game deals' key={key}>
+        
+        <h3>{deal.title}</h3>
+        <img src={deal.thumb} alt = "deal-game"/>
+        <h4>
+          <ul>
+          <li>Sale Price- ${deal.salePrice}</li>
+          <li>Normal Price - ${deal.normalPrice}</li>
+          <li>Total Savings - {deal.savings.substr(0,3)}%</li>
+        </ul>
+        </h4>
+        </div>
+      })}
+      </div>
 
     </div>
     </div>
