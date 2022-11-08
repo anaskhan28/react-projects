4b2001120965f5f3e34d99a6004272f259a2b436
anaskhan28 <khananasatique@gmail.com>
2022-11-08 10:45:51 +0530

    created cat custom hook

--- custom-hook/src/App.js
+++ custom-hook/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import { useToggle } from './useToggle';
-
+import { Cat } from './Cat';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 function App() {
+  const client = new QueryClient();
  const [isVisible, toggle] = useToggle()
   return (
     <div className="App">
+      <QueryClientProvider client={client}>
       <button onClick={toggle}>
         {isVisible? "Hide": "Show"}
       </button>
-      {isVisible && <h1>Hidden Text</h1>}
+      {isVisible && <Cat/> }
+      
+      </QueryClientProvider>
     </div>
+
   );
 }
 
---
+++ custom-hook/src/Cat.js
@@ -0,0 +1,13 @@
+import {useGetCat} from "./useGetCat";
+export const Cat = ()=>{
+   const {data, isCatLoading, refetchData} = useGetCat();
+   if(isCatLoading) return <h1>loading...</h1>
+    return (
+        <div>
+         
+        
+         <button onClick={refetchData}>Refetch</button>
+         <p>{data?.fact}</p>
+        </div>
+    );
+};
