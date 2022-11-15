15def2e6dc31d496ab2d903dda0b14b5d673316b
anaskhan28 <khananasatique@gmail.com>
2022-11-15 21:50:10 +0530

    env configuration

--- firebase-project/src/config/firebase.ts
+++ firebase-project/src/config/firebase.ts
@@ -2,19 +2,18 @@
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore';
-// import dotenv from 'dotenv'
-// dotenv.config()
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC5-wPAbYFGsii2PQWQ1fyDGs0ur8i4EJQ",
-  authDomain: "react-firebase-f3d2a.firebaseapp.com",
-  projectId: "react-firebase-f3d2a",
-  storageBucket: "react-firebase-f3d2a.appspot.com",
-  messagingSenderId: "93690537601",
-  appId: "1:93690537601:web:ad0ce8e0d90fe3ecad8f80"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
