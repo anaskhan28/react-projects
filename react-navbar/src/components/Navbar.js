ccc4e81e51b001d7e14c23a5ba8baddf663f401a
anaskhan28 <khananasatique@gmail.com>
2022-11-29 07:23:12 +0530

    navbar done and initialize hero component

--- react-navbar/src/components/Navbar.js
+++ react-navbar/src/components/Navbar.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Navbar.css';
 import {FaBars, FaTimes} from 'react-icons/fa';
 import Logo from '../images/logo.png';
 const Navbar = () =>{
+    const [click, setClick] = useState(false);
+
+    const handleClick = ()=> setClick(!click);
     return(
         <nav className='navbar'>
             <div className='logo'>
             <img src={Logo} alt="logo"/>
             </div>
-            <ul className='nav-menu'>
+            <ul className={click? 'nav-menu active': 'nav-menu'}>
             <li className='nav-item'><a href='/'>Home</a></li>
             <li className='nav-item'><a href='/'>About</a></li>
             <li className='nav-item'><a href='/'>FAQ</a></li>
             <li className='nav-item'><a href='/'>Contact</a></li>
             </ul>
-            <div className='hamburger'>
-                <FaBars />
+            <div className='hamburger' onClick={handleClick}>
+                {click?(<FaTimes size={30} style={{color:"#f8f8f8"}}/>)
+                : (<FaBars size={30} style={{color:"#f8f8f8"}}/>)}
+                
             </div>
         </nav>
     )
